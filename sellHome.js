1af36e1ece04286ca29f5a4fe3e58f801e94ad1f
xfekke <fekke4201337@gmail.com>
2023-11-28 12:21:05 +0100

    Update sellHome.js
    Uppdaterat så att flera av fälten måste ha ett värde för att kunna skapa ett bostads-obj.

--- sellHome.js
+++ sellHome.js
@@ -21,6 +21,17 @@ function sellResidence() {
                       <option value="fritidshus">Fritidshus</option>
                   </select>
               </div>
+              <div id="floorOption">
+                <label for="floor">Våning:</label>
+                <select id="floor" name="floor">
+                  <option value="none">Ingen</option>
+                  <option value="BV">BV</option>
+                  <option value="1">1</option>
+                  <option value="2">2</option>
+                  <option value="3">3</option>
+                </select>
+
+              </div>
               <div>
                   <label for="rooms">Antal rum:</label>
                   <input type="number" id="rooms" name="rooms" min="1" required>
@@ -33,10 +44,23 @@ function sellResidence() {
                   <label for="price">Utgångspris:</label>
                   <input type="number" id="price" name="price" required>
               </div>
+                 <label for="balcony">Balkong:</label>
+                  <select id="balcony" name="balcony">
+                      <option value="Ja">Ja</option>
+                      <option value="Nej">Nej</option>
+                  </select>
                   <div>
+                  <div>
+                  <label for="patio">Uteplats:</label>
+                  <select id="patio" name="patio">
+                      <option value="Ja">Ja</option>
+                      <option value="Nej">Nej</option>
+                  </select>
+                  </div>
                   <label for="image-url">Bild-URL:</label>
                   <input type="url" id="image-url" name="image-url" placeholder="Ange URL för bilden på din bostad">
               </div>
+
               <div>
                   <label for="additional-info">Ytterligare information:</label>
                   <textarea id="additional-info" name="additional-info"></textarea>
@@ -46,8 +70,22 @@ function sellResidence() {
       </div>
     `
   };
+
+
   return content;
 }
+
+// function toggleFloorOptions() {
+//   document.getElementById('type').addEventListener('change', toggleFloorOptions);
+//   var type = document.getElementById('type').value;
+//   var floorOptions = document.getElementById('floorOptions');
+
+//   if (type === 'lägenhet') {
+//     floorOptions.style.display = 'block';
+//   } else {
+//     floorOptions.style.display = 'none';
+//   }
+// }
 
 //submitResidence knapp
 
@@ -65,17 +103,35 @@ function updateContent() {
 function submitResidence() {
   var address = document.getElementById('address').value;
   var type = document.getElementById('type').value;
+  var floor = document.getElementById('floor').value;
   var rooms = document.getElementById('rooms').value;
   var size = document.getElementById('size').value;
   var price = document.getElementById('price').value;
+  var balcony = document.getElementById('balcony').value;
+  var patio = document.getElementById('patio').value;
+  var imageURL = document.getElementById('image-url').value;
   var additionalInfo = document.getElementById('additional-info').value;
+
+  if (
+    address === '' ||
+    rooms === '' ||
+    size === '' ||
+    price === ''
+  ) {
+    alert('Fyll i alla obligatoriska fält!');
+    return
+  }
 
   var formData = {
     address: address,
     type: type,
+    floor: floor,
     rooms: rooms,
     size: size,
     price: price,
+    balcony: balcony,
+    patio: patio,
+    imageURL: imageURL,
     additionalInfo: additionalInfo
   };
 
