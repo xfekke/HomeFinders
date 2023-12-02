040d07a94906f6d50631fa6cb6012a460c733d73
Jensju <jens@modulmedia.se>
2023-12-02 02:31:07 +0100

    Nattens sista push
    Nu med säljarna på plats

--- contact.js
+++ contact.js
@@ -4,13 +4,13 @@ function contactHF() {
 
     '#about': `
  <section id="brokers">
-  <h2 class="ingress"></h2>
+  <p class="ingress">Vill du komma i kontakt med våra vassa mäklare?</p>
 
     <div id="dealers">
       <!-- Mäklare 1 -->
       <div class="dealer">
         <img src="./kontakt/image/dealer.jpg" alt="Stig Insides" class="dealer-img">
-        <h3>Stig Insides</h3>
+        <h3 class="dealer-name">Stig Insides</h3>
         <p class="role">Auktoriserad bostadsmäklare</p>
         <p><a href="mailto:stig.insides@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
         <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
@@ -18,7 +18,7 @@ function contactHF() {
       <!-- Mäklare 2 -->
       <div class="dealer">
         <img src="./kontakt/image/dealer2.jpg" alt="Forentia Zales" class="dealer-img">
-        <h3>Forentia Zales</h3>
+        <h3 class="dealer-name">Forentia Zales</h3>
         <p class="role">Auktoriserad bostadsmäklare</p>
         <p><a href="mailto:forentia.zales@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
         <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
@@ -26,7 +26,7 @@ function contactHF() {
       <!-- Mäklare 3 -->
       <div class="dealer">
         <img src="./kontakt/image/dealer3.jpg" alt="Tille Salusson" class="dealer-img">
-        <h3>Tille Salusson</h3>
+        <h3 class="dealer-name">Tille Salusson</h3>
         <p class="role">Auktoriserad bostadsmäklare</p>
         <p><a href="mailto:tille.salusson@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
         <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
