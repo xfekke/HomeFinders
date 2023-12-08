a8c830e9adb72ea09ddc220a9797a24a6904ac88
xfekke <fekke4201337@gmail.com>
2023-12-08 16:24:57 +0100

    Update

--- public/views/contact.js
+++ public/views/contact.js
@@ -1,6 +1,46 @@
 import "../components/counter.js";
 
 export default () => /*html*/`
-    <h1>Kontakta oss</h1>
-    <p>Kontakta oss för mer information eller boka en visning.</p>'
-`;
+ <section id="brokers">
+  <p class="ingress">Vill du komma i kontakt med våra vassa mäklare?</p>
+
+    <div id="dealers">
+      <!-- Mäklare 1 -->
+      <div class="dealer">
+        <img src="dealer.jpg" alt="Stig Insides" class="dealer-img">
+        <h3 class="dealer-name">Stig Insides</h3>
+        <p class="role">Auktoriserad bostadsmäklare</p>
+        <p><a href="mailto:stig.insides@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+        <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+      </div>
+      <!-- Mäklare 2 -->
+      <div class="dealer">
+        <img src="dealer2.jpg" alt="Forentia Zales" class="dealer-img">
+        <h3 class="dealer-name">Forentia Zales</h3>
+        <p class="role">Auktoriserad bostadsmäklare</p>
+        <p><a href="mailto:forentia.zales@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+        <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+      </div>
+      <!-- Mäklare 3 -->
+      <div class="dealer">
+        <img src="dealer3.jpg" alt="Tille Salusson" class="dealer-img">
+        <h3 class="dealer-name">Tille Salusson</h3>
+        <p class="role">Auktoriserad bostadsmäklare</p>
+        <p><a href="mailto:tille.salusson@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+        <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+      </div>
+
+    </div>
+
+ </section>
+
+  <section class="form-section">
+       <p class="ingress">Du kan också skicka e-post till oss via formuläret</p>
+    <form action="">
+      <input type="email" id="email" placeholder="Ange e-postadress" name="email" required=true;>
+      <input type="messageBox" id="notation" placeholder="Ärende" name="notation" required=true;>
+      <input type="messageBox" id="message" placeholder="Skriv in ditt meddelande" name="message" required=true;>
+      <button id="form-btn">Skicka meddelandet</button>
+    </form>
+  </section>
+    `
