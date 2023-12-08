45485c765028e5d44bbee9bfcb1ed3fef24ee524
Jensju <jens@modulmedia.se>
2023-12-08 15:22:06 +0100

    Uppdaterad contact
    Med nya koden upplagd samt css

--- public/views/contact.js
+++ public/views/contact.js
@@ -1,6 +1,119 @@
 import "../components/counter.js";
 
+// export function sendUsAMessage() {
+//   const emailAddress = document.getElementById("email").value;
+//   const notation = document.getElementById("notation").value;
+//   const message = document.getElementById("message").value;
+
+//   console.log(`${emailAddress}\n${notation}\n${message}`);
+// }
+
 export default () => /*html*/`
     <h1>Kontakta oss</h1>
     <p>Kontakta oss för mer information eller boka en visning.</p>'
+
+   <h2>Kontaktformulär</h2>
+
+  <section class="form-section">
+       <p class="ingress">Du kan också skicka e-post till oss via formuläret</p>
+    <form action="">
+      <input type="email" id="email" placeholder="Ange e-postadress" name="email" required=true;>
+      <input type="text" id="notation" placeholder="Ärende" name="notation" required=true;>
+      <input type="text" id="message" placeholder="Skriv in ditt meddelande" name="message" required=true;>
+      <button id="form-btn">Skicka meddelandet</button>
+    </form>
+  </section>
 `;
+
+/*
+<section id="brokers">
+    <p class="ingress">Vill du komma i kontakt med våra vassa mäklare?</p>
+
+    <div id="dealers">
+        <!-- Mäklare 1 -->
+        <div class="dealer">
+            <img src="./kontakt/image/dealer.jpg" alt="Stig Insides" class="dealer-img">
+                <h3 class="dealer-name">Stig Insides</h3>
+                <p class="role">Auktoriserad bostadsmäklare</p>
+                <p><a href="mailto:stig.insides@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+        </div>
+        <!-- Mäklare 2 -->
+        <div class="dealer">
+            <img src="./kontakt/image/dealer2.jpg" alt="Forentia Zales" class="dealer-img">
+                <h3 class="dealer-name">Forentia Zales</h3>
+                <p class="role">Auktoriserad bostadsmäklare</p>
+                <p><a href="mailto:forentia.zales@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+        </div>
+        <!-- Mäklare 3 -->
+        <div class="dealer">
+            <img src="./kontakt/image/dealer3.jpg" alt="Tille Salusson" class="dealer-img">
+                <h3 class="dealer-name">Tille Salusson</h3>
+                <p class="role">Auktoriserad bostadsmäklare</p>
+                <p><a href="mailto:tille.salusson@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+        </div>
+
+    </div>
+
+</section>
+*/
+
+
+
+
+
+
+
+// function contactHF() {
+  
+//   var content = {
+
+//     '#about': `
+//  <section id="brokers">
+//   <p class="ingress">Vill du komma i kontakt med våra vassa mäklare?</p>
+
+//     <div id="dealers">
+//       <!-- Mäklare 1 -->
+//       <div class="dealer">
+//         <img src="./kontakt/image/dealer.jpg" alt="Stig Insides" class="dealer-img">
+//         <h3 class="dealer-name">Stig Insides</h3>
+//         <p class="role">Auktoriserad bostadsmäklare</p>
+//         <p><a href="mailto:stig.insides@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+//         <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+//       </div>
+//       <!-- Mäklare 2 -->
+//       <div class="dealer">
+//         <img src="./kontakt/image/dealer2.jpg" alt="Forentia Zales" class="dealer-img">
+//         <h3 class="dealer-name">Forentia Zales</h3>
+//         <p class="role">Auktoriserad bostadsmäklare</p>
+//         <p><a href="mailto:forentia.zales@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+//         <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+//       </div>
+//       <!-- Mäklare 3 -->
+//       <div class="dealer">
+//         <img src="./kontakt/image/dealer3.jpg" alt="Tille Salusson" class="dealer-img">
+//         <h3 class="dealer-name">Tille Salusson</h3>
+//         <p class="role">Auktoriserad bostadsmäklare</p>
+//         <p><a href="mailto:tille.salusson@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+//         <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+//       </div>
+
+//     </div>
+
+//  </section>
+
+//   <section class="form-section">
+//        <p class="ingress">Du kan också skicka e-post till oss via formuläret</p>
+//     <form action="">
+//       <input type="email" id="email" placeholder="Ange e-postadress" name="email" required=true;>
+//       <input type="text" id="notation" placeholder="Ärende" name="notation" required=true;>
+//       <input type="text" id="message" placeholder="Skriv in ditt meddelande" name="message" required=true;>
+//       <button id="form-btn">Skicka meddelandet</button>
+//     </form>
+//   </section>
+//     `
+//   }
+//   return content;
+// }
--- public/views/home.js
+++ public/views/home.js
@@ -1,6 +1,25 @@
 import "../components/counter.js";
 
 export default () => /*html*/`
-    <h2>Välkommen till Homefinders</h2>
-    <p>Din partner för att hitta drömboendet eller sälja din nuvarande bostad.</p>
+<main class="wrapper">
+
+    <div id="hero">
+        <img src="../image/hero1.jpg" class="hero-image">
+            <h1 class="titel">Väl!</h1>
+    </div>
+
+    <section id="First-sec">
+        <div class="content">
+            <p>Är du påväg att sälja eller köpa en bostad? Oavsett hur din resa ser ut är vi på Homefinders den partner som finns där i varje moment av din bostadsaffär. Våra vassa fastighetsmäklare hjälper dig med allt från bostadsvärdering, säljrådgivning till att hitta rätt köpare till din bostad. Letar du istället efter ett nytt hem så bistår vi med köprådgivning och matchar dina önskemål med pågående och kommande försäljningar. Hos oss hittar du extraordinära lägenheter, sjönära fritidshus, paradvillor utöver det vanliga, radhus med det det lilla extra och gårdar till försäljning. Kanske hittar du ditt nya boende hos oss! </p>
+        </div>
+    </section>
+    <footer></footer>
+</main>
 `;
+
+    // <h2>Välkommen till Homefinders</h2>
+    // <p>Din partner för att hitta drömboendet eller sälja din nuvarande bostad.</p>
+
+
+
+
