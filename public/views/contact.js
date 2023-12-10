273e71e6cc02bd0472a5698833da399208c45ed2
Jensju <jens@modulmedia.se>
2023-12-10 09:20:58 +0100

    Update
    Har flyttat in nav-texten in i header så och separerat dem lite och centrerat dem. Min hero-del skjuts av någon anledning för långt ner från header-delen. Har inte listat ut varför den gör så än.

--- public/views/contact.js
+++ public/views/contact.js
@@ -8,8 +8,39 @@ import "../components/counter.js";
 //   console.log(`${emailAddress}\n${notation}\n${message}`);
 // }
 
-export default () => /*html*/`
+export default () => /*html*/`  
+<section id="brokers">
+    <p class="ingress">Vill du komma i kontakt med våra vassa mäklare?</p>
 
+    <div id="dealers">
+        <!-- Mäklare 1 -->
+        <div class="dealer">
+            <img src="./image/dealer.jpg" alt="Stig Insides" class="dealer-img">
+                <h3 class="dealer-name">Stig Insides</h3>
+                <p class="role">Auktoriserad bostadsmäklare</p>
+                <p><a href="mailto:stig.insides@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+        </div>
+        <!-- Mäklare 2 -->
+        <div class="dealer">
+            <img src="./image/dealer2.jpg" alt="Forentia Zales" class="dealer-img">
+                <h3 class="dealer-name">Forentia Zales</h3>
+                <p class="role">Auktoriserad bostadsmäklare</p>
+                <p><a href="mailto:forentia.zales@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+        </div>
+        <!-- Mäklare 3 -->
+        <div class="dealer">
+            <img src="./image/dealer3.jpg" alt="Tille Salusson" class="dealer-img">
+                <h3 class="dealer-name">Tille Salusson</h3>
+                <p class="role">Auktoriserad bostadsmäklare</p>
+                <p><a href="mailto:tille.salusson@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
+                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
+        </div>
+
+    </div>
+
+</section>
 
    <h2>Kontaktformulär</h2>
 
@@ -30,39 +61,9 @@ export default () => /*html*/`
   </section>
 `;
 
-/*
-<section id="brokers">
-    <p class="ingress">Vill du komma i kontakt med våra vassa mäklare?</p>
 
-    <div id="dealers">
-        <!-- Mäklare 1 -->
-        <div class="dealer">
-            <img src="./kontakt/image/dealer.jpg" alt="Stig Insides" class="dealer-img">
-                <h3 class="dealer-name">Stig Insides</h3>
-                <p class="role">Auktoriserad bostadsmäklare</p>
-                <p><a href="mailto:stig.insides@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
-                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
-        </div>
-        <!-- Mäklare 2 -->
-        <div class="dealer">
-            <img src="./kontakt/image/dealer2.jpg" alt="Forentia Zales" class="dealer-img">
-                <h3 class="dealer-name">Forentia Zales</h3>
-                <p class="role">Auktoriserad bostadsmäklare</p>
-                <p><a href="mailto:forentia.zales@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
-                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
-        </div>
-        <!-- Mäklare 3 -->
-        <div class="dealer">
-            <img src="./kontakt/image/dealer3.jpg" alt="Tille Salusson" class="dealer-img">
-                <h3 class="dealer-name">Tille Salusson</h3>
-                <p class="role">Auktoriserad bostadsmäklare</p>
-                <p><a href="mailto:tille.salusson@homefinder.se?subject=Förfrågan om bostadsaffär">Maila mäklaren direkt!</a></p>
-                <p><a href="tel:+46 8 123 45 67">Ring mig: 08-123 45 67</a></p>
-        </div>
 
-    </div>
-
-</section>
+    /*
 */
 
 
