286117f013df3c45ae880722d20a885e1c8861c2
xfekke <fekke4201337@gmail.com>
2023-12-11 15:28:29 +0100

    Revert "Merge branch 'CSS' into dev"
    This reverts commit cf1d8a1f77765b503ee19c0301da29266c89ce68, reversing
    changes made to 651fee9edaa09beae9426944d921c6a545716f3f.

--- public/views/home.js
+++ public/views/home.js
@@ -1,28 +1,6 @@
 import "../components/counter.js";
 
 export default () => /*html*/`
-
-<div class="wrapper">
-    <div id="hero">
-        <img src="../image/hero1.jpg" class="hero-image">
-            <h1 class="titel">Välkommen till Homefinders!</h1>
-            <P class="p-hero">Din partner för att hitta drömboendet eller sälja din nuvarande bostad.</p>
-    </div>
-
-    <section id="First-sec">
-        <div class="content">
-            <p>Är du påväg att sälja eller köpa en bostad? Oavsett hur din resa ser ut är vi på Homefinders den partner som finns där i varje moment av din bostadsaffär. Våra vassa fastighetsmäklare hjälper dig med bostadsvärdering och säljrådgivning men även att hitta rätt köpare till just din bostad.</p>
-            <p>Är du istället på jakt ett nytt hem så bistår vi med köprådgivning och matchar dina önskemål med pågående och kommande försäljningar. Hos oss hittar du extraordinära lägenheter, sjönära fritidshus, paradvillor utöver det vanliga, radhus med det lilla extra och gårdar till försäljning. Kanske hittar du ditt drömboende via oss! </p>
-        </div>
-    </section>
-    
- </div>
-
+    <h2>Välkommen till Homefinders</h2>
+    <p>Din partner för att hitta drömboendet eller sälja din nuvarande bostad.</p>
 `;
-
-// <h2>Välkommen till Homefinders</h2>
-// <p>Din partner för att hitta drömboendet eller sälja din nuvarande bostad.</p>
-
-
-
-
